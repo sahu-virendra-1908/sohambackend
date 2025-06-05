e3b672f63df194b1a027d17226abe08caba2160b
sahu-virendra-1908 <virendrashivsahu@gmail.com>
2025-06-05 18:28:02 +0530

    change

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
